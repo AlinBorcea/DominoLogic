c0fc8a47722689520b30e25abacb87add4dc94cf
AlinBorcea <52531291+AlinBorcea@users.noreply.github.com>
2025-09-03 11:21:50 +0300

    instantiated a BlockLogic object

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import logic.BlockLogic;
+import logic.DominoLogic;
+import logic.strategies.SetFirstTileStrategy;
 
 public class Main {
+    private static DominoLogic dominoLogic;
+
     public static void main(String[] args) {
+        try {
+            dominoLogic = new BlockLogic(new SetFirstTileStrategy());
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(1);
+        } finally {
+            System.out.println(dominoLogic);
+        }
     }
 }
