270dd819224b8df91c8c83cb59a8fbaff57692a6
AlinBorcea <52531291+AlinBorcea@users.noreply.github.com>
2025-09-04 10:29:14 +0300

    added methods to run game turns and a failedTurns counter

--- src/logic/BlockLogic.java
+++ src/logic/BlockLogic.java
@@ -7,8 +7,32 @@ import java.util.List;
 
 public class BlockLogic implements DominoLogic {
     private final GameData gameData;
+    private int failedTurns = 0;
 
     public BlockLogic(List<String> playerNames, SetTileStrategy setTileStrategy, Table table) throws Exception {
         this.gameData = new GameData(playerNames, setTileStrategy, table);
+    }
+
+
+    @Override
+    public void runOneTurn() throws Exception {
+        try {
+            gameData.runTurn();
+            failedTurns = 0;
+        } catch (Exception e) {
+            gameData.goToNextPlayer();
+            failedTurns++;
+            if (failedTurns == gameData.playerCount()) {
+                throw new Exception("All player have failed to place pips");
+            }
+        }
+    }
+
+    @Override
+    public void runMultipleTurns(int numberOfTurns) throws Exception {
+        while (numberOfTurns > 0) {
+            runOneTurn();
+            numberOfTurns--;
+        }
     }
 }
