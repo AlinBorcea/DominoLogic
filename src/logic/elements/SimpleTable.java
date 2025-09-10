a0b9f7f873ddb683156bee8aced9ae1de081d1b6
AlinBorcea <52531291+AlinBorcea@users.noreply.github.com>
2025-09-10 15:57:37 +0300

    removed constructor and rewrote getTableScore

--- src/logic/elements/SimpleTable.java
+++ src/logic/elements/SimpleTable.java
@@ -7,11 +7,7 @@ import java.util.List;
 
 /// Table stores the tiles set be every player and calculates the table score.
 public class SimpleTable implements Table {
-    private final List<Tile> tiles;
-
-    public SimpleTable() {
-        this.tiles = new ArrayList<>();
-    }
+    private final List<Tile> tiles = new ArrayList<>();
 
     @Override
     public void setTileUsingStrategy(List<Tile> playerTiles, SetTileStrategy strategy) throws Exception {
@@ -30,6 +26,22 @@ public class SimpleTable implements Table {
     }
 
     public int getTableScore() {
-        return tiles.isEmpty() ? -1 : tiles.getFirst().valueA() + tiles.getLast().valueB();
+        if (tiles.isEmpty()) return 0;
+
+        var first = tiles.getFirst();
+        var last = tiles.getLast();
+        int score = first.valueA() + last.valueB();
+
+        if (tiles.size() == 1) return score;
+
+        if (first.isDouble()) {
+            score += first.valueA();
+        }
+
+        if (last.isDouble()) {
+            score += last.valueB();
+        }
+
+        return score;
     }
 }
